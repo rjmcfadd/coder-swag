fd14af9c5fe7811d40f16bad6ad5b75cf97cb11f
Ryan McFadden <rjmcfadd@gmail.com>
2017-11-09 11:44:04 -0500

    Lesson 3 finished

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Ryan McFadden on 11/9/17.
+//  Copyright © 2017 Ryan McFadden. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
